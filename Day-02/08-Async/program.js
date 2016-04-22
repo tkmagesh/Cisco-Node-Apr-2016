56a4185d6a5ca91d8fc77a456d49c5c5a8be8ffa
tkmagesh <tkmagesh77@gmail.com>
2016-04-22 10:37:08 +0530

    Async sequence - challenge - solution

--- Day-02/08-Async/program.js
+++ Day-02/08-Async/program.js
@@ -18,48 +18,64 @@ function f4Sync(){
 	console.log('f4Sync completed');
 }
 
+var syncFns = [f1Sync, f2Sync, f3Sync, f4Sync];
+
 function runSync(){
-	f1Sync();
-	f2Sync();
-	f3Sync();
-	f4Sync();
+	for(var i=0; i<syncFns.length; i++)
+		syncFns[i]();
 }
 
 module.exports.runSync = runSync;
 
-function f1Async(){
+function f1Async(next){
 	console.log('f1Async started');
 	setTimeout(function(){
 		console.log('f1Async completed');	
+		if (typeof next === 'function')
+			next();
 	}, 3000)
 }
 
-function f2Async(){
+function f2Async(next){
 	console.log('f2Async started');
 	setTimeout(function(){
 		console.log('f2Async completed');	
+		if (typeof next === 'function')
+			next();
 	}, 3000)
 }
 
-function f3Async(){
+function f3Async(next){
 	console.log('f3Async started');
 	setTimeout(function(){
 		console.log('f3Async completed');	
+		if (typeof next === 'function')
+			next();
 	}, 3000)
 }
 
-function f4Async(){
+function f4Async(next){
 	console.log('f4Async started');
 	setTimeout(function(){
 		console.log('f4Async completed');	
+		if (typeof next === 'function')
+			next();
 	}, 3000)
 }
 
+var asyncFns = [f1Async, f2Async, f3Async, f4Async];
+
 function runAsync(){
-	f1Async();
-	f2Async();
-	f3Async();
-	f4Async();
+	function exec(fns){
+		var first = fns[0],
+			remaining = fns.slice(1),
+			next = function(){
+				exec(remaining)
+			};
+		if (typeof first === 'function')
+			first(next);
+	}
+	exec(asyncFns);
 }
 
 module.exports.runAsync = runAsync;
